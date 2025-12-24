c7881448ba36ea5c3afcb84b01a0b463064787dd
Faizan <faizanjutt73@gmail.com>
2025-12-24 18:58:59 +0500

    reslve the auth errors errors

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -41,9 +41,8 @@ try {
   auth = getAuth(app);
 }
 
-// Configure Google Sign-In
 GoogleSignin.configure({
-  webClientId: '25460966938-m9v2v9n0b49ftsbmq453uvg65jpb135t.apps.googleusercontent.com', // Replace with your actual Web Client ID
+  webClientId: '25460966938-m9v2v9n0b49ftsbmq453uvg65jpb135t.apps.googleusercontent.com', 
   offlineAccess: true,
 });
 
